fa530596f91520e11015cbe86cc44dd7d16c3cc2
deltaTreize <ludo.leblond13@gmail.com>
2024-05-15 12:27:58 +0200

    db vercel activation

--- Front-End/src/TS-fichiers/pages/pagesUsers/userAccontPage/userAccontPage.tsx
+++ Front-End/src/TS-fichiers/pages/pagesUsers/userAccontPage/userAccontPage.tsx
@@ -64,7 +64,7 @@ export function UserAccontPage() {
 			 <section className="dashboard portefeuille-wrapper">
 				<Card />
 				<div className="chart-wrapper">
-					<h2 className="chart-title">répartition des dépenses</h2>
+					<h2 className="chart-title">Dépenses</h2>
 					<Chart />
 				</div>
 			</section>}
